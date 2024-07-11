cb5d885ca01b8482bb52fe34b5ca15c5259a95b0
Konstantin Ibadullaev <ki30cane@NB-konstantin.mathe.tu-freiberg.de>
2024-07-11 15:22:23 +0200

    added income class

---
+++ BudgetManager4U/Models/IncomeClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BudgetManager4U;
+public class IncomeClass
+{
+    public int id { get; set; }
+    public string category { get; set; }
+    public DateTime dateExp { get; set; }
+    public decimal valExp { get; set; }
+    public string description { get; set; }
+}
