e0e79087122c895c3b5d4308f34800ff3e7fa507
Konstantin Ibadullaev <ki30cane@NB-konstantin.mathe.tu-freiberg.de>
2024-07-11 16:14:03 +0200

    added properties to user acc class

--- BudgetManager4U/Models/UserAccountClass.cs
+++ BudgetManager4U/Models/UserAccountClass.cs
@@ -2,14 +2,24 @@
 namespace BudgetManager4U;
 public class UserAccountClass
 {
-	public string firstName { get; set; }	
-	public string secondName { get; set; }
-	public string email { get; set; }
-    private string registrationDate; 
+	public string firstName { get; set; }
+    public string lastName { get; set; }
+	
+    public string email { get; set; }
 
+    private string registrationDate; 
     public string RegistrationDate  
     {
         get { return registrationDate; }   
         set { registrationDate = value; }  
     }
+
+    private string password 
+    public string Password
+    {
+        get { return password; }
+        set { password = value; }
+    }
+
+
 }
