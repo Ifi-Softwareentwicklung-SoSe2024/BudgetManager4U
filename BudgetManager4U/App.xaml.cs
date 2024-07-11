3629b87960340ae0ee0dc671a8dd5ea33d5a57a5
Konstantin Ibadullaev <ki30cane@NB-konstantin.mathe.tu-freiberg.de>
2024-07-11 15:06:02 +0200

    created mvvm structure and added first model i.e expense class

---
+++ BudgetManager4U/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BudgetManager4U
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
