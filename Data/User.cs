6b6d9714135c88b447348128032dc3aee6ff02a5
Speed-Rabbit14 <63228435+Speed-Rabbit14@users.noreply.github.com>
2024-07-12 10:42:18 +0200

    SignUp & SignIn pages
    both pages work with SQLite database :)

---
+++ Data/User.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace BudgetManager4U.Data
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
