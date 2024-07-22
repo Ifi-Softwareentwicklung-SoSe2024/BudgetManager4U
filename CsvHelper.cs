e167b7a626b259b1ef0a107d07fbce7ebac78929
Speed-Rabbit14 <63228435+Speed-Rabbit14@users.noreply.github.com>
2024-07-22 14:44:17 +0200

    Csv Exporter
    >ExpenseClass & IncomeClass merged into one file TransactionClass
    >> TransactionClass with example data!
    >FileSaver and Button included (still needs to be edited for the final layout)

---
+++ CsvHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Text;
+
+public static class CsvHelper
+{
+    public static string ConvertTransactionsToCsv(List<Transaction> transactions)
+    {
+        var csvBuilder = new StringBuilder();
+        csvBuilder.AppendLine("Id,Value,Date,Description");
+
+        foreach (var transaction in transactions)
+        {
+            var line = $"{transaction.Id},{transaction.Value.ToString(CultureInfo.InvariantCulture)},{transaction.Date.ToString("yyyy-MM-dd")},{transaction.Description}";
+            csvBuilder.AppendLine(line);
+        }
+
+        return csvBuilder.ToString();
+    }
+}
