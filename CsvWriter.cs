b81285aa60c815992be9e291139d8ff9bb4d386d
Speed-Rabbit14 <63228435+Speed-Rabbit14@users.noreply.github.com>
2024-07-24 14:05:42 +0200

    Updated style documentation CSV Writer

--- CsvWriter.cs
+++ CsvWriter.cs
@@ -1,20 +1,25 @@
-﻿using System.Collections.Generic;   //Provides the 'List<T> collection class sued for the storing of transactions
-using System.IO;                    //Provides necessary 'StremWriter' for writing data to files
-using BudgetManager4U.Models;       // Contains 'TransactionClass'
+﻿using System.Collections.Generic;   
+using System.IO;                    
+using BudgetManager4U.Models;       
 
 public static class CsvWriter
 {
 
-    // Method to write transactions into a CSV file
+    /// <summary>
+    ///  method to formate and write transactions into a CSV file
+    /// </summary>
+    /// <param name="csv"></param>
+    /// <returns></returns>
+    
     public static void WriteTransactionsToCsv(List<TransactionClass> transactions, string filePath)
     {
         using (var writer = new StreamWriter(filePath))
         {
-            writer.WriteLine("Date,Description,TransactionAmount");     // Labeling the output (header)
+            writer.WriteLine("Date,Description,TransactionAmount");     
 
             foreach (var transaction in transactions)
             {
-                // formating and writing the transaction data (single line) to the CSV file
+                
                 writer.WriteLine($"{transaction.Datum:d},{transaction.Description},{transaction.TransactionAmount}"); 
             }
         }
